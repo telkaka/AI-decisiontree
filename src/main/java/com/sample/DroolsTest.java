ab55297122fc8ce965052cf80f1202db88241aca
Wojtek <wojtek.sipak@gmail.com>
2018-11-30 00:17:41 +0100

    Question class created

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -1,5 +1,8 @@
 package com.sample;
 
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
@@ -52,6 +55,75 @@ public class DroolsTest {
             this.status = status;
         }
 
+    }
+    public static class Question{
+    	/** TODO
+    	 * 
+    	 * use STATES for questions
+    	 * define actions after question is answered
+    	 * make an question-example to get rid of message example
+    	 * 
+		 */
+    	 
+    	
+    	private static final Logger LOGGER = Logger.getLogger( Question.class.getName() );
+    	public static final int UNAVAILABLE = 0;
+    	public static final int AVAILABLE = 1;
+    	public static final int ANSWERED = 2;
+    	
+    	private String content;
+    	private String[] answers;
+    	
+		private int status;
+    	
+    	public Question(String [] args) {
+    		// todo 
+    		// set status
+    		
+    		try {
+	    		this.setContent(args[0]);
+	    		this.setAnswers(Arrays.copyOfRange(args, 1, args.length));
+    		}
+    		catch(ArrayIndexOutOfBoundsException exception) {
+    			LOGGER.log(Level.FINE, "Not enough arguments in Question()", exception);
+    		}
+    	}
+    	
+    	public void Answer() {
+    		// todo
+    		// request answer from input, print the pick and change state
+    		System.out.println(this.getContent());
+    		String [] answers = this.getAnswers();
+    		int count = answers.length;
+    		for (int i=0;i< count ;i++) {
+    			System.out.println(i+1 + ") " + answers[i]);
+    		}
+    	}
+    	public String getContent() {
+			return content;
+		}
+
+		public void setContent(String content) {
+			this.content = content;
+		}
+
+		public String[] getAnswers() {
+			return answers;
+		}
+
+		public void setAnswers(String[] answers) {
+			this.answers = answers;
+		}
+
+		public int getStatus() {
+			return status;
+		}
+
+		public void setStatus(int status) {
+			this.status = status;
+		}
+
+    	
     }
 
 }
