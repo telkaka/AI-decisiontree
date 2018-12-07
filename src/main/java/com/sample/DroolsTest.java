50950c79c613ddcfc464aab5e7670a6172e1726a
Wojtek <wojtek.sipak@gmail.com>
2018-12-07 16:44:10 +0100

    all rules addes

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -19,43 +19,14 @@ public class DroolsTest {
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
 
-            // go !
-            Message message = new Message();
-            message.setMessage("Hello World");
-            message.setStatus(Message.HELLO);
-            kSession.insert(message);
+
             kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
 
-    public static class Message {
 
-        public static final int HELLO = 0;
-        public static final int GOODBYE = 1;
-
-        private String message;
-
-        private int status;
-
-        public String getMessage() {
-            return this.message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public int getStatus() {
-            return this.status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-
-    }
     public static class Question{
     	/** TODO
     	 * 
